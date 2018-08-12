f6528a77486b565db1873c3766567d56b4a14e30
Razmik Tovmasyan <razmikt@internal.synopsys.com>
2018-08-12 19:31:52 +0400

    Add Left and Right rotation methods

--- AVLTree.cs
+++ AVLTree.cs
@@ -2,6 +2,42 @@
 {
     class AVLTree
     {
+        private AVLNode root;
         
+        private AVLNode LeftRotation(AVLNode yNode)
+        //    y                            x
+        //   / \      Left Rotation       / \
+        //  T1  x    - – – – – – – >     y  T3
+        //     / \                      / \
+        //    T2  T3                   T1  T2
+        {
+            AVLNode xNode = yNode.Right;
+
+            yNode.Right = xNode.Left;
+            xNode.Left = yNode;
+
+            xNode.Height++;
+            yNode.Height--;
+
+            return xNode;
+        }
+
+        private AVLNode RightRotation(AVLNode yNode)
+        //      y                          x
+        //     / \     Right Rotation     / \
+        //    x   T3   – – – – – – – >   T1  y
+        //   / \                            / \
+        //  T1  T2                         T2  T3
+        {
+            AVLNode xNode = yNode.Left;
+
+            yNode.Left = xNode.Right;
+            xNode.Right = yNode;
+
+            xNode.Height++;
+            yNode.Height--;
+
+            return xNode;
+        }
     }
 }
