ea7f3a140f47773a18cc1ddc464753842cc42738
Razmik Tovmasyan <razmikt@internal.synopsys.com>
2018-08-12 18:23:39 +0400

    Add AVLTree and AVLNode files/classes
    AVLNode also has height

---
+++ AVLNode.cs
@@ -0,0 +1,21 @@
+﻿namespace BinarySearchTree
+{
+    class AVLNode
+    {
+        private int value;
+        private int height;
+        private AVLNode left;
+        private AVLNode right;
+
+        public AVLNode(int value)
+        {
+            Value = value;
+            Height = 1;
+        }
+
+        public int Value { get => value; set => this.value = value; }
+        public int Height { get => height; set => height = value; }
+        public AVLNode Left { get => left; set => left = value; }
+        public AVLNode Right { get => right; set => right = value; }
+    }
+}
