cf7341eddaf0f9ce07681dda94045260735e3d78
Razmik Tovmasyan <razmikt@internal.synopsys.com>
2018-08-12 20:36:42 +0400

    Add Insert method
    Insert uses InsertHelper method, which does BinarySearchTree insertion after that chacks and balances tree

--- AVLTree.cs
+++ AVLTree.cs
@@ -4,6 +4,62 @@
     {
         private AVLNode root;
 
+        public void Insert(int value) { root = InsertHelper(root, value); }
+
+        private AVLNode InsertHelper(AVLNode node, int value)
+        {
+            // Binary Search Tree insertion
+            if (node == null) return new AVLNode(value);
+
+            if(value < node.Value)
+            {
+                node.Left = InsertHelper(node.Left, value);
+            }
+            else if(value > node.Value)
+            {
+                node.Right = InsertHelper(node.Right, value);
+            }
+            else
+            // Duplicate value
+            {
+                return node;
+            }
+
+            // Update height of this node
+            node.Height = 1 + Max(Height(node.Left), Height(node.Right));
+
+            // Get the balance of this node
+            int balance = Balance(node);
+
+            if (balance > 1 && value < node.Left.Value)
+            //Left Left Case
+            {
+                return RightRotation(node);
+            }
+            else if (balance < -1 && value > node.Right.Value)
+            // Right Right Case
+            {
+                return LeftRotation(node);
+            }
+            else if(balance > 1 && value > node.Left.Value)
+            // Left Right Case
+            {
+                node.Left = LeftRotation(node.Left);
+                return RightRotation(node);
+            }
+            else if(balance < -1 && value < node.Right.Value)
+            // Right Left Case
+            {
+                node.Right = RightRotation(node.Right);
+                return LeftRotation(node);
+            }
+            else
+            // If subtree is balanced return this node
+            {
+                return node;
+            }
+        }
+
         private int Max(int a, int b) { return (a > b) ? a : b; }
         private int Height(AVLNode node) { return (node != null) ? node.Height : 0; }
         private int Balance(AVLNode node) { return (node != null) ? Height(node.Left) - Height(node.Right) : 0; }
