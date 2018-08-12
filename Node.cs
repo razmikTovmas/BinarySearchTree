1d6afb5a8d372e15013746585e5e3808cc756e4e
Razmik Tovmasyan <razmikt@internal.synopsys.com>
2018-08-12 13:15:08 +0400

    Add base node class

--- Node.cs
+++ Node.cs
@@ -2,6 +2,17 @@
 {
     class Node
     {
-        
+        private int value;
+        private Node left;
+        private Node right;
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get => value; set => this.value = value; }
+        public Node Left { get => left; set => left = value; }
+        public Node Right { get => right; set => right = value; }
     }
 }
