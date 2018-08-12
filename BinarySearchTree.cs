fbf6e43855e73796450d5439178d7bd613b94842
Razmik Tovmasyan <razmikt@internal.synopsys.com>
2018-08-12 13:54:14 +0400

    Add Insert and InsertNode methods

--- BinarySearchTree.cs
+++ BinarySearchTree.cs
@@ -1,7 +1,54 @@
-﻿namespace BinarySearchTree
+﻿using System;
+
+namespace BinarySearchTree
 {
     class BinarySearchTree
     {
+        private Node root;
 
+        public Node Insert(int value)
+        {
+            Node node = new Node(value);
+
+            if (root == null)
+            {
+                root = node;
+                return root;
+            }
+            else
+            {
+                return InsertNode(root, node);
+            }
+        }
+
+        private Node InsertNode(Node current, Node node)
+        {
+            if(node.Value < current.Value)
+            {
+                if(current.Left == null)
+                {
+                    current.Left = node;
+                    return node;
+                }
+                else
+                {
+                    return InsertNode(current.Left, node);
+                }
+            }
+            else if (node.Value > current.Value)
+            {
+                if(current.Right == null)
+                {
+                    current.Right = node;
+                    return node;
+                }
+                else
+                {
+                    return InsertNode(current.Right, node);
+                }
+            }
+
+            return null;
+        }
     }
 }
